3c59ed43c9bd7bf35d4343e81b268f769b3d4578
Nur <olimovnurullo2001@gmail.com>
2022-05-23 18:32:05 +0500

    Added menu сверстал фрагменты

--- app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/AddFragment.kt
+++ app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.example.to_doappcleanarchitecture.presentation.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.to_doappcleanarchitecture.R
 
 
@@ -15,7 +13,13 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
--- app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/ListFragment.kt
+++ app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/ListFragment.kt
@@ -1,11 +1,12 @@
 package com.example.to_doappcleanarchitecture.presentation.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import com.example.to_doappcleanarchitecture.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
@@ -15,7 +16,24 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        val floatButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        floatButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        val listLayout = view.findViewById<ConstraintLayout>(R.id.list_layout)
+        listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 }
