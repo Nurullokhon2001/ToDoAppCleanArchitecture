63f5719806c7875ca52a68d1c09ae638c9a63165
Nur <qalay84@gmail.com>
2022-06-09 18:16:09 +0500

    refactoring

---
+++ app/src/main/java/com/example/to_doappcleanarchitecture/data/model/ToDoData.kt
@@ -0,0 +1,14 @@
+package com.example.to_doappcleanarchitecture.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.to_doappcleanarchitecture.presentation.model.Priority
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+)
