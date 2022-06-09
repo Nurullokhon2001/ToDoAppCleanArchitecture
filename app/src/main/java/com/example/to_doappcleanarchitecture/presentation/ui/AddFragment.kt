ba81713e9a6224b2dedb3e4c76457358644ba3e1
Nur <qalay84@gmail.com>
2022-06-09 18:16:19 +0500

    refactoring

--- app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/AddFragment.kt
+++ app/src/main/java/com/example/to_doappcleanarchitecture/presentation/ui/AddFragment.kt
@@ -2,24 +2,77 @@ package com.example.to_doappcleanarchitecture.presentation.ui
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.to_doappcleanarchitecture.R
+import com.example.to_doappcleanarchitecture.data.model.ToDoData
+import com.example.to_doappcleanarchitecture.databinding.FragmentAddBinding
+import com.example.to_doappcleanarchitecture.presentation.vm.SharedViewModel
+import com.example.to_doappcleanarchitecture.presentation.vm.ToDoViewModel
 
 
 class AddFragment : Fragment() {
 
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+ private val mSharedViewModel: SharedViewModel by viewModels()
+
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+
+        // Set Menu
         setHasOptionsMenu(true)
-        return view
+
+        // Spinner Item Selected Listener
+      binding.spPriority.onItemSelectedListener = mSharedViewModel.listener
+
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.check_to_do){
+            insertDataToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDb() {
+        val mTitle = binding.etTitle.text.toString()
+        val mPriority = binding.spPriority.selectedItem.toString()
+        val mDescription = binding.etDescription.text.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+        if(validation){
+            // Insert Data to Database
+            val newData = ToDoData(
+                0,
+                mTitle,
+                mSharedViewModel.parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            // Navigate Back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
